2998de09f1e2ff25a5b1caef3285bdcffd2c82e8
Daniel Figucio <affrae@users.noreply.github.com>
2019-11-05 17:59:00 +1100

    Update index.js

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ function* getSequence(theStart = 0) {
   while (true) yield getNumber(theStart++)
 }
 
-/*
+
 function badFunction() {
   
   var upper = 4;
@@ -59,5 +59,5 @@ function badFunction() {
     a[i] = 0;
     
 }
-*/
+
 
